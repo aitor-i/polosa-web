c1bb20e89dddf0335e9a196c870e6dae42ef5f98
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-06-06 18:24:42 +0200

    ADD - Home page

---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import Navigation from "components/Navigation";
+
+export const HomePage = () => {
+  return (
+    <main>
+      <Navigation />
+      <hgroup>
+        <h1>Polosa</h1>
+        <h2>Habit tracker</h2>
+      </hgroup>
+    </main>
+  );
+};
