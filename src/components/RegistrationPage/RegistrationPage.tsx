e1089416abd222886595552aa9fed8872036a1e3
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-06-07 21:03:04 +0200

    ADD - Registration page

---
+++ src/components/RegistrationPage/RegistrationPage.tsx
@@ -0,0 +1,31 @@
+import Navigation from "components/Navigation";
+
+import styles from "./RegistrationPage.module.css";
+
+export const RegistrationPage = () => {
+  return (
+    <main>
+      <Navigation />
+      <h1>Sign in</h1>
+      <form className={styles.form}>
+        <span className={styles.inputContainer}>
+          <label htmlFor="">Name</label>
+          <input type="text" />
+        </span>
+        <span className={styles.inputContainer}>
+          <label htmlFor="">Password</label>
+          <input type="password" />
+        </span>
+        <span className={styles.inputContainer}>
+          <label htmlFor="">Confirm Password</label>
+          <input type="password" />
+        </span>
+        <span className={styles.buttonContainer}>
+          <button className={styles.submit} type="submit">
+            Register
+          </button>
+        </span>
+      </form>
+    </main>
+  );
+};
