54f67f5c9fde666140b0e147e7a7c767e56fcfc3
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-06-15 19:58:10 +0200

    ADD - Icons array

---
+++ src/domain/icons/Icons.tsx
@@ -0,0 +1,14 @@
+import { FaBookOpen, FaTree, FaWineGlass, FaChessKnight } from 'react-icons/fa';
+import { MdOutlineSmokeFree, MdTv, MdSportsGymnastics, MdOutlinePhonelinkRing, MdGTranslate } from 'react-icons/md';
+
+export const Icons = [
+  { name: 'book', icon: <FaBookOpen /> },
+  { name: 'tree', icon: <FaTree /> },
+  { name: 'glass', icon: <FaWineGlass /> },
+  { name: 'chess', icon: <FaChessKnight /> },
+  { name: 'smoke', icon: <MdOutlineSmokeFree /> },
+  { name: 'gym', icon: <MdSportsGymnastics /> },
+  { name: 'phone', icon: <MdOutlinePhonelinkRing /> },
+  { name: 'language', icon: <MdGTranslate /> },
+  { name: 'tv', icon: <MdTv /> }
+];
