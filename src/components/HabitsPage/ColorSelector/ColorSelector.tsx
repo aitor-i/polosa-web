9b1a6a698b3e68a6f33a492740bd40589f7fcc33
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-06-19 17:24:58 +0200

    UPDATE - Color picker

--- src/components/HabitsPage/ColorSelector/ColorSelector.tsx
+++ src/components/HabitsPage/ColorSelector/ColorSelector.tsx
@@ -9,30 +9,33 @@ type Color = (typeof habitColors.colors)[0];
 export const ColorSelector = () => {
   const [color, setColor] = useState<Color>(habitColors.colors[0]);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isRendered, setIsRendered] = useState(false);
 
   const colorClickHandler = (event: MouseEvent<HTMLParagraphElement>) => {
     event.preventDefault();
 
+    setIsRendered(true);
     setIsModalOpen(true);
   };
 
   const closeModal = () => {
     setIsModalOpen(false);
   };
-  const backDropClickHandler = (event: MouseEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    closeModal();
-  };
 
   const selectColorClickHandler = (color: Color) => {
     setColor(color);
+    setIsRendered(false);
+  };
+
+  const onAnimationEndHandler = () => {
+    if (isModalOpen && !isRendered) closeModal();
   };
 
   return (
-    <div>
+    <div className={` ${isRendered ? styles.colorSelectorContainer : styles.close}`}>
       <p onClick={colorClickHandler} key={color.name} className={styles.color} style={{ backgroundColor: color.code }}></p>
       {isModalOpen ? (
-        <div onClick={backDropClickHandler} className={styles.backdrop}>
+        <div className={`${isRendered ? styles.backdrop : styles.collapse}`} onAnimationEnd={onAnimationEndHandler}>
           <section className={styles.paletteContainer}>
             {habitColors.colors.map(color => (
               <p onClick={() => selectColorClickHandler(color)} key={color.name} className={styles.color} style={{ backgroundColor: color.code }}></p>
